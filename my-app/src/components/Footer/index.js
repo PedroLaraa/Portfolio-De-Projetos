6ace6d59036ab4f7aea1ae95c0dda87cde868730
Pedro Lara <pedroalveslara@gmail.com>
2022-09-16 16:21:08 -0300

    V1.0 =D

--- my-app/src/components/Footer/index.js
+++ my-app/src/components/Footer/index.js
@@ -1,9 +1,26 @@
+import DeveloperSocialMedia from '../DeveloperSocialMedia';
 import './Footer.css'
 
 const Footer = () => {
 
     return(
-        <h1>Footer</h1>
+        <footer>
+            <div className='footerContainer'>
+                <section className='infosFooter'>
+                    <div>
+                        <p>
+                            © 2022 Copyright: Pedro Alves Lara
+                        </p>
+                    </div>
+                </section>
+                <section className='socialMediaFooter'>
+                    <DeveloperSocialMedia />
+                </section>
+            </div>
+            <div className='toTopBtn'>
+                <button className='btn btn-dark'><a href='#home'>▲</a></button>
+            </div>
+        </footer>
     )
 
 };
